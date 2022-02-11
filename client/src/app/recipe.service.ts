b0ce16487b8f0f27524d5b3b86ccecb94f2e9ec0
Shiraj <muhamadshiraj@gmail.com>
2022-02-11 17:13:49 +0800

    final commit2

--- client/src/app/recipe.service.ts
+++ client/src/app/recipe.service.ts
@@ -13,14 +13,16 @@ export class RecipeService {
 
   getAllRecipes(): Promise<RecipeSummary[]> {
     return lastValueFrom(
-      this.http.get<RecipeSummary[]>('http://localhost:8080/api/recipes')
+      this.http.get<RecipeSummary[]>('/api/recipes')
+      // this.http.get<RecipeSummary[]>('http://localhost:8080/api/recipes')
     );
 
   }
 
   getRecipe(id: string): Promise<Recipe> {
     return lastValueFrom(
-      this.http.get<Recipe>('http://localhost:8080/api/recipe/'.concat(id))
+      this.http.get<Recipe>('/api/recipe/'.concat(id))
+      // this.http.get<Recipe>('http://localhost:8080/api/recipe/'.concat(id))
     )
   }
 }
